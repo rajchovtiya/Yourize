8545f44c9969d65cdb109260c807b8bef3557f75
rajchovatiya <rajchovatiya783@gmail.com>
2025-08-07 09:49:46 +0530

    code complete

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './commponets/ScrollToTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <ScrollToTop />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
